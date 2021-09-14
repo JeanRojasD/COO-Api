b10767b3c02cee7b50bf191869a4e728d24744ce
JeanRojasD <71621140+JeanRojasD@users.noreply.github.com>
2021-09-13 21:50:26 -0300

    projeto inicial

---
+++ src/main/java/com/br/cooapi/CooApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.cooapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CooApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/cooapi/CooApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.cooapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
