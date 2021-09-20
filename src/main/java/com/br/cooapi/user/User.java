00e542a23387dcdd15881501a5cf652f3616cd18
JeanRojasD <71621140+JeanRojasD@users.noreply.github.com>
2021-09-20 20:31:22 -0300

    crud de usuários sem a implementação de regra de negocios

--- src/main/java/com/br/cooapi/user/User.java
+++ src/main/java/com/br/cooapi/user/User.java
@@ -1,10 +1,15 @@
 package com.br.cooapi.user;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.br.CPF;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 
 @Entity
 @Data
@@ -12,12 +17,18 @@ public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @CPF
     private String cpf;
     private String name;
+    @Email
     private String email;
     private String phone;
     private String senha;
 
-
+    public static User from(UserForm userForm){
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper.map(userForm, User.class);
+    }
 
 }
--- src/main/java/com/br/cooapi/user/UserController.java
+++ src/main/java/com/br/cooapi/user/UserController.java
@@ -1,5 +1,41 @@
 package com.br.cooapi.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll(){
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody @Valid UserForm userForm){
+        return ResponseEntity.ok(userService.create(userForm));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody @Valid UserForm userForm){
+        return ResponseEntity.ok(userService.update(id, userForm));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.ok().build();
+    }
 }
