5d5e4c24d2036f99585d00f735505956561d8814
gustavo <gustavoantonio.pp@outlook.com>
2021-09-27 19:10:57 -0300

    crud brand

---
+++ src/main/java/com/br/cooapi/brand/BrandDTO.java
@@ -0,0 +1,20 @@
+package com.br.cooapi.brand;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+
+@Data
+public class BrandDTO {
+
+    private Long id;
+    private String marca;
+
+    public static BrandDTO from (Brand brand){
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper.map(brand, BrandDTO.class);
+    }
+}
