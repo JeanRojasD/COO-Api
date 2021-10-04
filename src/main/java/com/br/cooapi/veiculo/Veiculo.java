2c0dd01e556dcb1230721b1a7d5c973ebeaa876c
Eduardo Azevedo <69330807+TheRealLactose@users.noreply.github.com>
2021-10-04 16:32:42 -0300

    test unit

--- src/main/java/com/br/cooapi/veiculo/Veiculo.java
+++ src/main/java/com/br/cooapi/veiculo/Veiculo.java
@@ -6,10 +6,7 @@ import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
@@ -20,6 +17,8 @@ public class Veiculo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idveiculo;
+
+    //@UniqueConstraint()
     private String placa;
     private Integer ano;
 
