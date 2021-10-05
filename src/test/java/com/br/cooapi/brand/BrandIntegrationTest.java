1c2e246da731ca373f1f66576ef849aadb4a298e
gustavo <gustavoantonio.pp@outlook.com>
2021-10-05 17:24:20 -0300

    Brand Integration Test

--- src/test/java/com/br/cooapi/brand/BrandIntegrationTest.java
+++ src/test/java/com/br/cooapi/brand/BrandIntegrationTest.java
@@ -1,10 +1,24 @@
 package com.br.cooapi.brand;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class BrandIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     public void IntTestCreate () throws Exception {
