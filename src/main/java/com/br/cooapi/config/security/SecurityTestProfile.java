fed85584768bd1635986ec6e8ba795265238f7be
JeanRojasD <71621140+JeanRojasD@users.noreply.github.com>
2021-10-06 19:58:27 -0300

    ajuste no swagger

--- src/main/java/com/br/cooapi/config/security/SecurityTestProfile.java
+++ src/main/java/com/br/cooapi/config/security/SecurityTestProfile.java
@@ -2,16 +2,22 @@ package com.br.cooapi.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Profile("test")
 @Configuration
+@EnableWebSecurity
 public class SecurityTestProfile extends WebSecurityConfigurerAdapter {
 
     @Override
-    public void configure(WebSecurity webSecurity) throws Exception{
-        webSecurity.ignoring().anyRequest();
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and().csrf().disable()
+                .formLogin().disable();
+        ;
     }
 
 }
