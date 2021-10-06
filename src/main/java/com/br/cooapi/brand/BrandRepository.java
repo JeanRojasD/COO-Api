daa56c9c844deb70ffadff486eb9184d4e0e0741
Jean Rojas <71621140+JeanRojasD@users.noreply.github.com>
2021-10-06 20:03:59 -0300

    Revert "Swagger configuration"

--- src/main/java/com/br/cooapi/brand/BrandRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.cooapi.brand;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface BrandRepository extends JpaRepository<Brand, Long> {
-    Optional<Brand> findByMarcaContaining(String marca);
-}
--- src/main/java/com/br/cooapi/model/ModelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.cooapi.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface ModelRepository extends JpaRepository<Model, Long> {
-    Optional<Model> findByModeloContaining(String modelo);
-}
