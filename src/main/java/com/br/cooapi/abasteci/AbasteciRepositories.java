726055c2c348b9b722bfe8def8a9c1a1e5075607
Bruno <brunodornelesdeabreu@hotmail.com>
2021-10-16 19:13:39 -0300

    teste

--- src/main/java/com/br/cooapi/abasteci/AbasteciRepositories.java
+++ src/main/java/com/br/cooapi/abasteci/AbasteciRepositories.java
@@ -4,7 +4,10 @@ package com.br.cooapi.abasteci;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AbasteciRepositories extends JpaRepository<Abasteci, Integer> {
 
+    Optional<Abasteci> findByVlPago (String teste);
 }
--- src/test/java/com/br/cooapi/abasteci/AbasteciTest.java
+++ src/test/java/com/br/cooapi/abasteci/AbasteciTest.java
@@ -1,8 +1,11 @@
 package com.br.cooapi.abasteci;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -33,6 +36,29 @@ public class AbasteciTest {
         Abasteci postUpdate = repositories.save(abasteci1);
 
         assertEquals(postUpdate.getVlPago(), teste);
+    }
+
+    @Test
+    public void testDelete(){
+        Abasteci abasteci =new Abasteci();
+
+        abasteci.setVlPago(teste);
+        repositories.save(abasteci);
+
+        Abasteci abasteci1 =repositories.findById(1).get();
+
+        repositories.delete(abasteci1);
+
+        Abasteci abasteci2 = null;
+
+        Optional<Abasteci>optionalAbasteci = repositories.findByVlPago("teste");
+
+        if(optionalAbasteci.isPresent()){
+            abasteci2=optionalAbasteci.get();
+
+        }
+        Assertions.assertThat(abasteci2).isNull();
+
     }
 
 }
